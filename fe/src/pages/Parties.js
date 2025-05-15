b73ea8b621664edeab40de0e264c5d94c9b06e11
Shreyansh <kake0shreyansh@gmail.com>
2025-05-15 13:44:25 +0530

    update

--- fe/src/pages/Parties.js
+++ fe/src/pages/Parties.js
@@ -4,7 +4,7 @@ import Comp2 from '../components/comp2';
 import Wave from '../components/Wave2';
 import axios from 'axios';
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000';
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL ;
 
 const Parties = () => {
   const [partyData, setPartyData] = useState({
